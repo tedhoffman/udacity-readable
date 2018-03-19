7eca19d5255a0a0e5459fdc2f9c7bc96ff3d6b72
Ted Hoffman <tedhoffman@users.noreply.github.com>
2018-03-19 11:50:52 -0400

    Change API

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,6 @@
 import React, { Component } from 'react'
 import { fetchData } from '../utils/api'
 
-/*
-
-Post detail is available at /:category/:post_id
-
-Post is displayed with the following:
-1) Title
-2) Body
-3) Author
-4) Number of comments
-5) Current score
-6) Voting mechanism to upvote or downvote the post
-7) Buttons or links for editing or deleting that post
-
-Listed comments are displayed with the following:
-1) Author
-2) Current score
-3) Voting mechanism to upvote or downvote the comment
-4) Buttons or links for editing or deleting that comment
-
-The voting mechanism works and correctly displays the new vote score after clicking for both the post and comments.
-
-All comments for a post are displayed below the post body.
-
-A mechanism for adding a new comment is visible on the detail page and functional.
-*/
-
 class App extends Component {
 	componentDidMount() {
 		console.log(fetchData('categories', 'GET'))
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,14 @@
-/*
-
-export function fetchRecipes (food = '') {
-  food = food.trim()
-
-  return fetch(`https://localhost:$q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
-    .then((res) => res.json())
-    .then(({ hits }) => hits.map(({ recipe }) => recipe))
-}
-
-*/
-
-export function fetchData (what = '', method = 'GET') {
+export function fetchData (call = '', method = 'GET') {
+	let output
 	let headers = {
 		'authorization': 'asdf',
 		'content-type': 'application/json',
-		'method': method
 	}
 
-	return fetch(`http://localhost:3001/${what}`, { headers })
-		.then((result) => result.json())
+	fetch(`http://localhost:3001/${call}`, { headers })
+		.then((response) => {
+			response.json().then((data) => {
+				return data
+		})
+	})
 }
